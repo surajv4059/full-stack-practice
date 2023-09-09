823949178d93b86744fcd38e3abe7d02bafb2ffe
surajv4059 <suraj12.116@gmail.com>
2023-09-09 19:19:47 +0530

    implemented file based course app backend

--- courses-app/in-memory/index.js
+++ courses-app/in-memory/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
 let ADMINS = []
+fs.readFile("ADMINS.json","utf-8",(err,data) => {
+    ADMINS = JSON.parse(data);
+})
 let COURSES = []
+fs.readFile("COURSES.json","utf-8",(err,data) => {
+    COURSES = JSON.parse(data);
+})
 let USERS = []
+fs.readFile("USERS.json","utf-8",(err,data) => {
+    USERS = JSON.parse(data);
+})
 
 function adminAuthentication(req,res,next){
     const {username,password} = req.headers;
@@ -36,30 +48,6 @@ function userAuthentication(req,res,next){
 }
 
 
-
-app.get("/admin/courses",adminAuthentication,(Req,res) => {
-    res.json({courses:COURSES});
-})
-
-app.post("/admin/courses" , adminAuthentication, (req,res) => {
-    const course = req.body;
-    course.id = Date.now();
-    COURSES.push(course);
-    res.json({message:"course added successfully" , courseID:course.id});
-})
-
-app.put("/admin/courses/:courseId",adminAuthentication, (req,res) => {
-    const courseId = parseInt(req.params.courseId);   
-    const course = COURSES.find(c => c.id === courseId);
-    if(course){
-        Object.assign(course,req.body);
-        res.json({message:"course updated successfully"});
-    }
-    else{
-        res.status(404).json({message:"course not found"});
-    }
-}) 
-
 app.post("/admin/signup",(req,res) => {
     const admin = req.body;
     const existingAdmin = ADMINS.find( a =>a.username === admin.username);
@@ -68,19 +56,63 @@ app.post("/admin/signup",(req,res) => {
     }
     else{
         ADMINS.push(admin);
-        res.json(ADMINS);
+        fs.writeFile("ADMINS.json",JSON.stringify(ADMINS), (err) => {
+            if(err) throw err;
+            res.json({message:"admin added successfully"});
+        });
+        
     }
 })
+
 
 app.post("/admin/login",adminAuthentication,(req,res) => {
     res.json("logged in successfully");
 })
 
 
+app.post("/admin/courses" , adminAuthentication, (req,res) => {
+    const course = req.body;
+    course.id = Date.now();
+    COURSES.push(course);
+    fs.writeFile("COURSES.json",JSON.stringify(COURSES), (err) => {
+        if(err) throw err;
+        res.json({message:"course added successfully" , courseID:course.id});
+    });
+})
+
+
+app.get("/admin/courses",adminAuthentication,(Req,res) => {
+    res.json({courses:COURSES});
+})
+
+
+app.put("/admin/courses/:courseId",adminAuthentication, (req,res) => {
+    const courseId = parseInt(req.params.courseId);   
+    const course = COURSES.find(c => c.id === courseId);
+    if(course){
+        Object.assign(course,req.body);
+
+        fs.writeFile("COURSES.json",JSON.stringify(COURSES), (err) => {
+            if(err) throw err;
+            res.json({message:"course updated successfully"});
+        });
+    }
+    else{
+        res.status(404).json({message:"course not found"});
+    }
+}) 
+
+
+
+
+
 app.post("/user/signup",(req,res) => {
     const user = {...req.body , purchasedCourses: [] };
     USERS.push(user);
-    res.send({message:"user created"});
+    fs.writeFile("USERS.json",JSON.stringify(USERS), (err) => {
+        if(err) throw err;
+        res.send({message:"user created"});
+    });
 })
 
 app.post("/user/login",userAuthentication,(req,res) => {
@@ -96,9 +128,11 @@ app.post("/user/courses/:courseId",userAuthentication,(req,res) => {
     const course = COURSES.find(c => c.id ===courseId && c.published);
     if(course){
         req.user.purchasedCourses.push(courseId);
-        res.json({message:"course purchased successfully"});
+        fs.writeFile("USERS.json",JSON.stringify(USERS), (err) => {
+            if(err) throw err;
+            res.json({message:"course purchased successfully"});
+        });
     }
-
     else{
         res.json({mesage:"could not find course or course not availabile"});
     }
